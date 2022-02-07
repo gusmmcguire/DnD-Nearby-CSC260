b7ab6a4471bb96eceb792a7d364605ade498694f
Gus <gusmcg1856@gmail.com>
2022-02-07 09:31:57 -0700

    Add project files.

---
+++ DnD Nearby/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DnD_Nearby.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
