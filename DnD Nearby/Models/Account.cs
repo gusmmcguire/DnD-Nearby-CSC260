3b2bfb4fde259b8c4e59d4f693261fc149b35597
Gus <gusmcg1856@gmail.com>
2022-02-14 08:48:00 -0700

    added account.cs model

---
+++ DnD Nearby/Models/Account.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace DnD_Nearby.Models
+{
+    public class Account
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Username")]
+        [Required]
+        public string Username { get; set; }
+
+        [BsonElement("Password")]
+        [Required]
+        public string Password { get; set; }
+        [BsonElement("DisplayName")]
+        public string DisplayName { get; set; } = "";
+        [BsonElement("FirstName")]
+        [Required]
+        public string FirstName { get; set; }
+        [BsonElement("LastName")]
+        [Required]
+        public string LastName { get; set; }
+
+        public string FullName { get { return FirstName + " " + LastName; } }
+    }
+}
