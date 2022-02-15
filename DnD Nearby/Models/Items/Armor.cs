22006afbc79811127aad5c86bce6ea9e30c7a0ed
Peter Ouzts <81652748+pouzts@users.noreply.github.com>
2022-02-15 09:29:45 -0700

    2/15/2022 - Added base Item Models (Item, Equipment, Armor, Weapon, Food, and Tool).

---
+++ DnD Nearby/Models/Items/Armor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DnD_Nearby.Models
+{
+    public class Armor : Equipment
+    {
+        public int ACMod { get; set; }
+
+        public Armor(int id, string name, float cost, string url, bool isEquip, int modifier, int acMod): base(id, name, cost, url, isEquip, modifier)
+        {
+            this.ACMod = acMod;
+        }
+
+        public override void Equip(ref int[] attributes, int modAttribIndex)
+        {
+            if (IsEquipped) return;
+
+            IsEquipped = true;
+            attributes = ModStats(attributes, modAttribIndex);
+        }
+
+        public override void Unequip(ref int[] attributes, int modAttribIndex)
+        {
+            if (!IsEquipped) return;
+
+            IsEquipped = false;
+            attributes = ModStats(attributes, modAttribIndex);
+        }
+    }
+}
