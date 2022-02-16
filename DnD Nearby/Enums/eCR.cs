d64d2ca47fdab8e862ba904c7d2c79c224616bc3
Marltonthegreat <75112858+Marltonthegreat@users.noreply.github.com>
2022-02-16 09:14:35 -0700

    2/16/22 eCR creation

---
+++ DnD Nearby/Enums/eCR.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DnD_Nearby.Enums
+{
+    //Continue later
+    public enum eCR
+    {
+        Zero = 0,
+        OneEighth = 25,
+        OneFourth = 50,
+        OneHalf = 100,
+        One = 200,
+        Two = 450,
+        Three = 700,
+        Four = 1100,
+        Five = 1800,
+        Six = 2300,
+        Seven = 2900,
+        Eight = 3900,
+        Nine = 5000,
+        Ten = 5900,
+        Eleven = 7200,
+        Twelve = 8400,
+        Thirteen = 10000,
+        Fourteen = 11500,
+        Fifteen = 13000,
+        Sixteen = 15000,
+        Seventeen = 18000,
+        Eighteen = 20000,
+        Nineteen = 22000,
+        Twenty = 25000,
+        TwentyOne = 33000,
+        TwentyTwo = 41000,
+        TwentyThree = 50000,
+        TwentyFour = 62000,
+        Thirty = 115000,
+    }
+}
