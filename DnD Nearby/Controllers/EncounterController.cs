01cb3573a356b85dc82832c079968f6d362a7fdc
Marltonthegreat <75112858+Marltonthegreat@users.noreply.github.com>
2022-02-23 08:55:00 -0700

    2/23/22 Initial Encounter Controller

---
+++ DnD Nearby/Controllers/EncounterController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using DnD_Nearby.Models;
+
+namespace DnD_Nearby.Controllers
+{
+    public class EncounterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Encounter()
+        {
+            Encounter encounter = new Encounter();
+            Creature[] creatures =
+            {
+                new StatBlock(Enums.eCR.Twenty, "Goosenator", "Sweater", new int[] { 20, 20, 20, 20, 20, 20}, 10000, 35, new List<string>() { "Common" }),
+                new StatBlock(Enums.eCR.Twenty, "Isaianator", "Sweater", new int[] { 20, 20, 20, 20, 20, 20}, 10000, 35, new List<string>() { "Common" }),
+                new StatBlock(Enums.eCR.Twenty, "Carlonator", "Sweater", new int[] { 20, 20, 20, 20, 20, 20}, 10000, 35, new List<string>() { "Common" }),
+                new StatBlock(Enums.eCR.Twenty, "Jarenator", "Sweater", new int[] { 20, 20, 20, 20, 20, 20}, 10000, 35, new List<string>() { "Common" }),
+                new StatBlock(Enums.eCR.Twenty, "Petenator", "Sweater", new int[] { 20, 20, 20, 20, 20, 20}, 10000, 35, new List<string>() { "Common" })
+            };
+
+            foreach (Creature creature in creatures)
+            {
+                encounter.AddCreature(creature);
+            }
+
+            return View(encounter);
+        }
+    }
+}
