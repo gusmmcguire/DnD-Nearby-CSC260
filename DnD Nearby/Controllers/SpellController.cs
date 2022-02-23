b89bee70dfba5c56095413223072fc166c3be55b
carlosbarajas19 <cbarajas@student.neumont.edu>
2022-02-23 09:43:16 -0700

    02-23-2022 Added a spell view which displays a temporary spell created in the spell controller.

---
+++ DnD Nearby/Controllers/SpellController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DnD_Nearby.Models;
+
+namespace DnD_Nearby.Controllers
+{
+    public class SpellController : Controller
+    {
+        Spell tmpSpell = new Spell
+            (
+                "Acid Splash", //name
+                "Conjuration", //school
+                "1 Action", //casting time
+                "60 Feet", //range
+                "Instantaneous", //duration
+                new string[] { "V", "S" }, //components 
+                new string[] { "Artificer", "Sorcerer", "Wizard" }, //acuired by
+                "You hurl a bubble of acid. Choose one creature you can see within range, or choose two creatures you can see within range that are within 5 feet of each other. A target must succeed on a Dexterity saving throw or take 1d6 acid damage." +
+                " At Higher Levels.This spell’s damage increases by 1d6 when you reach 5th level(2d6), 11th level(3d6), and 17th level(4d6)."
+            );
+        List<Spell> tmpSpellList = new List<Spell>();
+        public IActionResult Index()
+        {
+            return View("/Home/Index");
+        }
+
+        public IActionResult SpellPage()
+        {
+            return View(tmpSpell);
+        }
+        [HttpPost]
+        public IActionResult GetSpellPage(int Id)
+        {
+            var spell = tmpSpellList.Find(s => s.ID == Id);
+            return View("SpellPage", spell);
+        }
+    }
+}
