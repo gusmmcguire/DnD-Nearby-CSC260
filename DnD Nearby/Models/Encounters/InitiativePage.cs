8e2fded82ba5a9390b6a0c02eefd2829a83225e7
Marltonthegreat <75112858+Marltonthegreat@users.noreply.github.com>
2022-03-09 19:21:00 -0700

    3/9/22 Initiative Tracker progress Pushing so as to work on PC instead of laptop

--- DnD Nearby/Models/Encounters/InitiativePage.cs
+++ DnD Nearby/Models/Encounters/InitiativePage.cs
@@ -1,13 +1,25 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using DnD_Nearby.Enums;
 
 namespace DnD_Nearby.Models
 {
     public class InitiativePage
     {
-        public InitiativeTracker initTracker;
+        public InitiativeTracker initTracker { get; set; } = new InitiativeTracker();
+        public Encounter encounter { get; set; } = new Encounter();
+
+        public InitiativePage() { }
+        public InitiativePage(Encounter en)
+        {
+            DiceRoller diceRoller = new DiceRoller();
+            encounter = en;
+            foreach (Creature creature in encounter.Creatures)
+            {
+                initTracker.AddCreature(creature, diceRoller.RollDice(20) + creature.CalcAttributeMod(1));
+            }
+        }
     }
 }
